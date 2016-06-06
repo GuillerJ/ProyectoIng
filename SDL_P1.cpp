ebb5de1345c54c3b31ab170b2499ea0e1fcd459a
Guillermo Javier Vivar Lúcia <guillerjps@hotmail.com>
2016-06-06 10:02:14 +0200

    Odontologic V2.3.1 	Version estable antes de sacar el codigo en funciones a parte.

--- SDL_P1.cpp
+++ SDL_P1.cpp
@@ -12,7 +12,8 @@
 * V2.1 Bool para imagen vista para que la ayuda sepa si hay o no imagen cargada. Boton de mentionano cogido. Añadido un icono a la aplicación
 * V2.2 Ahora si esta activado el boton mentoniano el mandibular se apaga y viceversa. La función de mandibular solo deja pintar 2 lineas y se desactiva
 * V2.2.1	Si la imagen no está cargada no deja pintar
-* v2.3 Incluida la función de mentoniano ya pinta un cruceta para señalarlo
+* v2.3 Incluida la función de mentoniano ya pinta una cruceta para señalarlo
+* V2.3.1	//poner OR en el mentoniano y cmand para luego sacar las rectas en esta misma función
 
 * TODO:
 * 
@@ -37,7 +38,7 @@ int main(int, char**) {
 	// crea la ventana
 	SDL_Window *win = SDL_CreateWindow("Odontologic V1.4.1", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_BORDERLESS);
 	if (win == nullptr) {
-		std::cerr << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
+		std::cerr << "SDL_CreateWindow \n\tError: " << SDL_GetError() << std::endl;
 		SDL_Quit();
 		return 1;
 	}
@@ -46,7 +47,7 @@ int main(int, char**) {
 	SDL_Renderer *ren = SDL_CreateRenderer(win, -1, 1);
 	if (ren == nullptr) {
 		cleanup(win); // SDL_DestroyWindow(win);
-		std::cerr << "SDL_CreateRenderer Error: " << SDL_GetError() << std::endl;
+		std::cerr << "SDL_CreateRenderer \n\tError: " << SDL_GetError() << std::endl;
 		SDL_Quit();
 		return 1;
 	}
@@ -56,7 +57,7 @@ int main(int, char**) {
 	SDL_Surface *Icono = SDL_LoadBMP(PathIcono.c_str());
 	if (Icono == nullptr) {
 		cleanup(ren, win); // SDL_DestroyRenderer(ren); SDL_DestroyWindow(win);
-		std::cerr << "SDL_LoadBMP del Icono de la aplicacion. Error: " << SDL_GetError() << std::endl;
+		std::cerr << "Error al cargar el Icono de la aplicacion. \n\tCodigo de Error de SDL_LoadBMP: " << SDL_GetError() << std::endl;
 		SDL_Quit();
 		return 1;
 	}
@@ -76,7 +77,7 @@ int main(int, char**) {
 	SDL_Texture *texPanel = SDL_CreateTextureFromSurface(ren, Panel);
 	if (texPanel == nullptr) {
 		cleanup(ren, win); // SDL_DestroyRenderer(ren); SDL_DestroyWindow(win);
-		std::cerr << "SDL_CreateTextureFromSurface de la GUI Error: " << SDL_GetError() << std::endl;
+		std::cerr << "Error al cargar la imagen GUI \n\tCodigo de Error de SDL_CreateTextureFromSurface: " << SDL_GetError() << std::endl;
 		SDL_Quit();
 		return 1;
 	}
@@ -128,6 +129,16 @@ int main(int, char**) {
 	rectMand.y = 51;
 	rectMand.w = 9;
 	rectMand.h = 9;
+
+	//----BISECTRIZ--------
+	float dAC = 0;
+	float dBA = 0;
+	int X = 0;
+	int Y = 0;
+	int a1, a2, b1, b2, c1, c2;
+	//----BISECTRIZ END----
+
+
 	//*****************Variables END*******************
 
 	while (!quit) {
@@ -167,7 +178,7 @@ int main(int, char**) {
 					SDL_Surface* Loading_Surf = SDL_LoadBMP("Ortopantomografia/OP1.bmp");
 					if (Loading_Surf == nullptr) {
 						cleanup(ren, win);
-						std::cerr << "Error al cargar la Ortopantomografía. Codigo de Error: " << SDL_GetError() << std::endl;
+						std::cerr << "Error al cargar la Ortopantomografia. \n\tCodigo de Error: " << SDL_GetError() << std::endl;
 						SDL_Quit();
 						return 1;
 					}
@@ -211,7 +222,7 @@ int main(int, char**) {
 				if ((e.button.x > 750 && e.button.x < 777) & (e.button.y > 0 && e.button.y < 24)) {
 					SDL_MinimizeWindow(win);
 				}
-				//BOTON Mentoniano
+				//BOTON Mentoniano		CRUCETA
 				if ((imagenOn == true) & (e.button.x > 249 && e.button.x < 371) & (e.button.y > 46 && e.button.y < 67)) {
 					cMand = false;
 					SDL_SetRenderDrawColor(ren, 255, 0, 0, 255);
@@ -249,8 +260,64 @@ int main(int, char**) {
 
 				//Panel Para pintar lineas
 				if (((cMand == true) || (mentoniano == true)) & (e.button.x > 10 && e.button.x < 790) & (e.button.y > 80 && e.button.y < 590)) {
-					//poner OR en el mentoniano y cmand para luego sacar las rectas en esta misma función
+					
+					
+					
+					if (cMand == true){
+						if (vecesPintar == 0) {
+							std::cout << " 1 ";
+							x1 = e.button.x;
+							y1 = e.button.y;
+						}
+						if (vecesPintar == 1) {
+							std::cout << " 2 ";
+							x2 = e.button.x;
+							y2 = e.button.y;
+							SDL_RenderDrawLine(ren, x1, y1, x2, y2);
+						}
+						b1 = x1;
+						b2 = y1;
+						a1 = x2;
+						a2 = y2;
+						vecesPintar++;
+						vecesMand++;
+						if (vecesPintar == 3) {
+							x2 = e.button.x;
+							y2 = e.button.y;
+							SDL_RenderDrawLine(ren, a1, a2, x2, y2);
+							vecesPintar = 0;
+						}
+						c1 = x2;
+						c2 = y2;
 
+
+						float A[2] = { a1,a2 }, B[2] = { b1,b2 }, C[2] = { c1,c2 };
+
+
+						dBA = sqrt(((a1 - b1) ^ 2) + ((a2 - b2) ^ 2));
+						dAC = sqrt((c1 - a1) ^ 2 + (c2 - a2) ^ 2);
+						float r = dAC / dBA;
+
+						float  t[] = { X,Y };
+
+						X = (b1 + r*c1) / (1 + r);
+						Y = (b2 + r*c2) / (1 + r);
+						std::cout << "(" << X << "," << Y << ")";
+
+
+						SDL_RenderDrawLine(ren, a1, a2, X, Y);
+
+
+						if (vecesMand == 4) {
+							vecesMand = 0;
+							cMand = false;
+							SDL_SetRenderDrawColor(ren, 255, 0, 0, 255);
+							SDL_RenderFillRect(ren, &rectMand);
+						}
+					}
+
+
+					/*
 					if (cMand == true){
 						if (vecesPintar == 0) {
 							std::cout << " 1 ";
@@ -268,16 +335,17 @@ int main(int, char**) {
 						if (vecesPintar == 2)
 							vecesPintar = 0;
 						if (vecesMand == 4) {
+							vecesMand = 0;
 							cMand = false;
 							SDL_SetRenderDrawColor(ren, 255, 0, 0, 255);
 							SDL_RenderFillRect(ren, &rectMand);
 						}
-					}
+					}*/
 					if (mentoniano == true) {
 						x1 = e.button.x;
 						y1 = e.button.y;
-						SDL_RenderDrawLine(ren, x1, y1 + 200, x1, y1 - 200);
-						SDL_RenderDrawLine(ren, x1 + 200, y1, x1 - 200, y1);
+						SDL_RenderDrawLine(ren, x1, y1 + 100, x1, y1 - 100);
+						SDL_RenderDrawLine(ren, x1 + 100, y1, x1 - 100, y1);
 
 					}
 				}
